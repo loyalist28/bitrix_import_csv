264c26e3f1e722335abcca43263ba01fe9d4aa03
loyalist28 <loyalist282@gmail.com>
2022-11-18 15:24:38 +1000

    release

--- csv_import.php
+++ csv_import.php
@@ -1,11 +1,68 @@
-<?php 
-require_once($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php');
+<?php
+use \Bitrix\Main\Loader;
 
-CModule::IncludeModule('iblock');
+set_time_limit(0);
 
-$importFile = $_SERVER['DOCUMENT_ROOT'] . '/dev-import/test.csv';
-$iBlockId = 5;
-$iBlockName = ((CIBlock::GetByID($iBlockId))->GetNext())['NAME'];
+define('NO_KEEP_STATISTICS', true);
+define("NO_AGENT_CHECK", true);
+define('NOT_CHECK_PERMISSIONS', true);
+
+// Установка запущен ли скрипт из CLI
+if(PHP_SAPI == 'cli') {
+    $cliRun = true;
+
+    define('BX_BUFFER_USED', true);
+} else {
+    $cliRun = false;
+}
+
+// Если скрипт запущен из CLI, и $_SERVER['DOCUMENT_ROOT'] отсутствует, 
+// то потребовать передать его третьим параметром запуска скрипта.
+if($cliRun) {
+    if(empty($_SERVER['DOCUMENT_ROOT'])) {
+        if(!isset($argv[2])) {
+            exit('Ошибка: требуется передать третьим параметром запуска $_SERVER["DOCUMENT_ROOT"] для запускаемого скрипта');
+        }
+        $_SERVER['DOCUMENT_ROOT'] = $argv[3];
+    }
+}
+
+$documentRoot = $_SERVER['DOCUMENT_ROOT'];
+
+require_once($documentRoot . '/bitrix/modules/main/include/prolog_before.php');
+if($cliRun) {
+    while (ob_get_level()) {
+        ob_end_flush();
+    }
+}
+
+Loader::IncludeModule('iblock');
+
+$importFile = '';
+$iBlockId = false;
+if($cliRun) {
+    if(!isset($argv[1])) {
+        exit('Ошибка: не указан файл импорта.');
+    } else {
+        $importFile = $documentRoot . '/' . $argv[1];
+    }
+    if(!isset($argv[2])) {
+        exit('Ошибка: не указан ID инфоблока.');
+    } else {
+        $iBlockId = intval($argv[2]);
+    }
+} else {
+    if(empty($_GET['i_file'])) {
+        exit('<h3>Ошибка: не указан файл импорта</h3>');
+    } else {
+        $importFile = $documentRoot . '/' . $_GET['i_file'];
+    }
+    if(empty($_GET['ib_id'])) {
+        exit('<h3>Ошибка: не указан ID инфоблока</h3>');
+    } else {
+        $iBlockId = intval($_GET['ib_id']);
+    }
+}
 $fieldsAssocArr = array(
     'XML_ID' => 'id',
     'NAME' => 'name',
@@ -19,7 +76,7 @@ $propsAssocArr = array(
 
 if(($importElementsArr = parse_csv_to_import_arr($importFile, $fieldsAssocArr, $propsAssocArr)) !== false) {
     if(!empty($importElementsArr)) {
-        $result = array();
+        $result = array('added' => [], 'add_faults' => [], 'updated' => [], 'update_faults' => []);
         $iBlockElement = new CIBlockElement;
         $propsSelectArr = array();
         foreach(array_keys($propsAssocArr) as $propertyCode) {
@@ -86,13 +143,12 @@ if(($importElementsArr = parse_csv_to_import_arr($importFile, $fieldsAssocArr, $
                 }
             }
         }
-    } else {
-        echo 'Нет элементов для импорта и обновления';
     }
 } else {
-    echo 'Ошибка: не удалось выполнить импорт';
+    exit();
 }
-?>
+// Запуск в браузере
+if(!$cliRun) : ?>
 <div id="import-block">
     <style>
         #import-block {
@@ -128,7 +184,7 @@ if(($importElementsArr = parse_csv_to_import_arr($importFile, $fieldsAssocArr, $
         }
     </style>
 
-    <h1>Импорт элементов в инфоблок "<?= $iBlockName ?>"</h1>
+    <h1>Импорт элементов в инфоблок</h1>
     <p>
         Источник: <b><?= $importFile ?></b><br>
         Разделитель ячеек: <b>;</b><br>
@@ -136,8 +192,10 @@ if(($importElementsArr = parse_csv_to_import_arr($importFile, $fieldsAssocArr, $
     <h3>Отчет по импорту:</h3>
 
     <div id="import-results">
-<?php if(!empty($result)) : ?>
-    <?php if(isset($result['added'])) : ?>
+
+    <?php if(!empty($result)) : ?>
+        <?php if(!empty($result['added'])) : ?>
+
         <div id="added-elements" class="report-block">
             <div class='description'>
                 <span>Элементов импортировано: <b><?= count($result['added']) ?></b>.</span> <span class="toggler show">Показать</span>
@@ -150,8 +208,10 @@ if(($importElementsArr = parse_csv_to_import_arr($importFile, $fieldsAssocArr, $
             ?>
             </ul>
         </div>
-    <?php endif;
-    if(isset($result['add_faults'])) : ?>
+
+        <?php endif;
+        if(!empty($result['add_faults'])) : ?>
+
         <div id="add-faults" class="report-block">
             <div class="description">
                 <span>Не удалось импортировать элементов: <b><?= count($result['add_faults']) ?></b>.</span> <span class="toggler show">Показать</span>
@@ -164,8 +224,10 @@ if(($importElementsArr = parse_csv_to_import_arr($importFile, $fieldsAssocArr, $
             ?>
             </ul>
         </div>
-    <?php endif;
-    if(isset($result['updated'])) : ?>
+
+        <?php endif;
+        if(!empty($result['updated'])) : ?>
+
         <div id="updated-elements" class="report-block">
             <div class="description">
                 <span>Элементов обновлено: <b><?= count($result['updated']) ?></b>.</span> <span class="toggler show">Показать</span>
@@ -178,8 +240,10 @@ if(($importElementsArr = parse_csv_to_import_arr($importFile, $fieldsAssocArr, $
             ?>
             </ul>
         </div>
-    <?php endif; 
-    if(isset($result['update_faults'])) : ?>
+
+        <?php endif; 
+        if(!empty($result['update_faults'])) : ?>
+
         <div id="update-faults" class="report-block">
             <div class="description">
                 <span>Не удалось обновить элементов: <b><?= count($result['update_faults']) ?></b>.</span> <span class="toggler show">Показать</span>
@@ -192,11 +256,12 @@ if(($importElementsArr = parse_csv_to_import_arr($importFile, $fieldsAssocArr, $
             ?>
             </ul>
         </div>
-    <?php endif;
-else :
-    echo 'Нет элементов для импорта и обновления';
-endif;
-?>
+
+        <?php endif;
+    else :
+        echo 'Нет элементов для импорта и обновления';
+    endif;
+    ?>
     </div>
     <script>
         document.getElementById('import-results').addEventListener('click', function(event) {      
@@ -218,6 +283,95 @@ endif;
     </script>
 </div>
 <?
+// Запуск в CLI
+else:
+    echo 'Импорт элементов в инфоблок' . PHP_EOL;
+    echo 'Источник: ' . $importFile  . PHP_EOL;
+    echo 'Разделитель ячеек: ;'  . PHP_EOL . PHP_EOL;
+    echo '== Отчет по импорту ==' . PHP_EOL;
+
+    if(!empty($result)) {
+        $stdin = fopen('php://stdin', 'r');
+        $detailOrderItems = array();
+
+        if(!empty($result['added'])) {
+            echo 'Элементов импортировано: ' . count($result['added']) . PHP_EOL;
+            $detailOrderItems[] = 'added'; 
+        }
+        if(!empty($result['add_faults'])) {
+            echo 'Не удалось импортировать элементов: ' . count($result['add_faults']) . PHP_EOL;
+            $detailOrderItems[] = 'add_faults';
+        }
+        if(!empty($result['updated'])) {
+            echo 'Элементов обновлено: ' . count($result['updated']) . PHP_EOL;
+            $detailOrderItems[] = 'updated'; 
+        }
+        if(!empty($result['update_faults'])) {
+            echo 'Не удалось обновить элементов: ' . count($result['update_faults']) . PHP_EOL;
+            $detailOrderItems[] = 'update_faults'; 
+        }
+
+        echo PHP_EOL . 'Показать детальный отчет? (y / n)' . PHP_EOL;
+
+        while(($input = strtolower(trim(fgets($stdin), PHP_EOL))) != 'n') {
+            if($input != 'y') {
+                echo PHP_EOL . 'Некорректный ввод!' . PHP_EOL;
+                echo PHP_EOL . 'Показать детальный отчет? (y / n)' . PHP_EOL;
+            } else {
+                print_order_items_list($detailOrderItems);
+
+                $lastListItemNumber = count($detailOrderItems) + 1;
+
+                while(($itemNumber = trim(fgets($stdin), PHP_EOL)) != $lastListItemNumber) {
+                    if(!is_numeric($itemNumber) || $itemNumber > $lastListItemNumber) {
+                        echo PHP_EOL . 'Указан неверный номер пункта!' . PHP_EOL;
+    
+                        print_order_items_list($detailOrderItems);
+                    } else {
+                        $choosenItem = $detailOrderItems[$itemNumber - 1];
+                        $strTemplate = '';
+                        switch ($choosenItem) {
+                            case 'added': 
+                                $strTemplate = '- Элемент с внешним кодом %d. ID нового элемента инфоблока: %d' . PHP_EOL;
+                                echo PHP_EOL . "Импортированые элементы:" . PHP_EOL;
+            
+                                break;
+                            case 'add_faults': 
+                                $strTemplate = '- Элемент с внешним кодом %d.' . PHP_EOL . 'Ошибка: %s';
+                                echo PHP_EOL . "Не удалось импортировать:" . PHP_EOL;
+            
+                                break;
+                            case 'updated': 
+                                $strTemplate = '- Элемент с внешним кодом %d. ID элемента: %d' . PHP_EOL;
+                                echo PHP_EOL . "Обновленные элементы:" . PHP_EOL; 
+            
+                                break;
+                            case 'update_faults': 
+                                $strTemplate = '- Элемент с внешним кодом %d.' . PHP_EOL . 'Ошибка: %s';
+                                echo PHP_EOL . "Не удалось обновить:" . PHP_EOL;
+            
+                                break;
+                        }
+            
+                        echo str_repeat('-', 30) . PHP_EOL;
+                        foreach($result[$choosenItem] as $key => $value) {
+                            $value = str_replace('<br>', PHP_EOL, $value);
+                            
+                            printf($strTemplate, $key, $value);
+                        }
+                        echo str_repeat('-', 30) . PHP_EOL;
+                        
+                        print_order_items_list($detailOrderItems);
+                    }
+                }
+                
+                break;
+            }
+        } 
+    } else {
+        echo 'Нет элементов для импорта и обновления';
+    }
+endif;
 
 /**
  * Преобразует csv-файл импорта в массив элементов для импорта в инфоблок. 
@@ -279,8 +433,42 @@ function parse_csv_to_import_arr($importFile, $fieldsAssocArr, $propsAssocArr) {
         
         return $importElementsArr;
     } else {
-        echo 'Ошибка: невозможно открыть файл ' . $importFile;
+        echo '<h3>Ошибка: невозможно открыть файл ' . $importFile . '</h3>';
 
         return false;
     }
+}
+/**
+ * Выводит список пунктов для детального показа отчета по импорту при запуске скрипта из CLI. 
+ * 
+ * @param   array  $detailOrderItems   Массив с пунктами отчета
+ */
+function print_order_items_list($detailOrderItems) {
+    echo PHP_EOL . 'Выберите пункт для детализации отчета: ' . PHP_EOL;
+
+    foreach($detailOrderItems as $i => $item) {
+        $itemNum = $i + 1;
+        
+        switch($item) {
+            case 'added':
+                echo "{$itemNum}. Импортированые элементы" . PHP_EOL;
+
+                break;
+            case 'add_faults':
+                echo "{$itemNum}. Не удалось импортировать" . PHP_EOL;
+
+                break;
+            case 'updated':
+                echo "{$itemNum}. Обновленные элементы" . PHP_EOL; 
+
+                break;
+            case 'update_faults':
+                echo "{$itemNum}. Не удалось обновить" . PHP_EOL;
+
+                break;
+        }
+    }
+
+    echo (count($detailOrderItems) + 1) . '. Завершить работу' . PHP_EOL;
+    echo PHP_EOL . 'Введите номер пункта: ';
 }
